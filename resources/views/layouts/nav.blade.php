f4698950e8d88f84702b6b360df36dec35ab7db2
Yohan Salamone <yohan.salamone@outlook.com>
2021-01-28 15:04:28 +0100

    ajout authentification

--- resources/views/layouts/nav.blade.php
+++
@@ -1,9 +0,0 @@
-<nav class="flex justify-between p-8 border-b">
-
-    <h3><a href="{{ route('home') }}">Laravel</a></h3>
-    <div class="flex">
-        <a class="px-4 hover:text-blue-600" href="#">Lien 1</a>
-        <a class="px-4 hover:text-blue-600" href="#">Lien 2</a>
-    </div>
-
-</nav>
