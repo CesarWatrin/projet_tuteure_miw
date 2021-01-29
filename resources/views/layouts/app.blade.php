c301baec978514f48cc501c46858516fc07c0747
Yohan Salamone <yohan.salamone@outlook.com>
2021-01-29 17:01:28 +0100

    intégration header index + sprite svg illustration login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/index.css') }}" rel="stylesheet">
     <link href="{{ asset('css/map.css') }}" rel="stylesheet">
     <link href="{{ asset('css/connect.css') }}" rel="stylesheet">
      <link href="{{ asset('css/navbar.css') }}" rel="stylesheet">
