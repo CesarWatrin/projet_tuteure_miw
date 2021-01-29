98bdca5f7a1538cbf39640c34ba0c4f2618504a8
maxBDL <maxime_didier@aol.com>
2021-01-29 16:08:55 +0100

    navbar search

--- resources/views/pages/map.blade.php
+++ resources/views/pages/map.blade.php
@@ -2,15 +2,14 @@
 
 
 @section('content')
-    <div id="map"></div>
-    <!--<script>
-        let carte = L.map('carte', {center: [46.3630104, 2.9846608],zoom: 5, attributionControl : false});
-        /* On aurait aussi pu écrire
-        let carte = L.map('maCarte').setView([46.3630104, 2.9846608],5 );
-        */
-        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'}).addTo(carte);
-        /*let marker = L.marker([44.566667,6.083333]);
-        marker.addTo(carte);*/
+    <div class="searchbar">
+        <div class="search">
+            <input type="text" placeholder="Essayer un nom de ville ...">
+            <button></button>
+        </div>
+        <button class="filter"></button>
+    </div>
 
-    </script>-->
+
+    <div id="map"></div>
 @endsection
