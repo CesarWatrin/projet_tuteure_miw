ce8a4a1790987fb206608096bd2715fda63970cb
maxBDL <maxime_didier@aol.com>
2021-01-29 14:48:27 +0100

    zoom controll

--- resources/js/map.js
+++ resources/js/map.js
@@ -1,2 +1,3 @@
-let carte = L.map('map', {center: [46.3630104, 2.9846608],zoom: 5, attributionControl : false});
+let carte = L.map('map', {center: [46.3630104, 2.9846608],zoom: 5, attributionControl : false, zoomControl: false});
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'}).addTo(carte);
+L.control.zoom({position:'bottomright'}).addTo(carte);
