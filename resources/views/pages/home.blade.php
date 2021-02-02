8075cb062a7a6278dafc6f4d5b6883855c5485e3
CesarWatrin <cesar.watrin@hotmail.com>
2021-02-02 16:08:12 +0100

    Merge branch 'main' of https://github.com/CesarWatrin/projet_tuteure_miw into main

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,20 +4,22 @@
 
     <header class="index_header">
         <span class="corp">MAC-YO Corp.</span>
-        <img class="main_illustration" src="images/main_illustration.svg"/>
-        <div class="flex_container">
-            <h1 class="accroche">Ventes<br>à emporter ?<span>C'est ici.</span></h1>
-            <form>
-                <input type="text" class="inputVille" placeholder="Essayez un nom de ville..."/>
-                <button class="bouton recherche">
-                    <svg class="icon"><use xlink:href="images/sprite.svg#loupe"></use></svg>
+        <div class="header_content">
+            <img class="main_illustration" src="images/main_illustration.svg"/>
+            <div class="flex_container">
+                <h1 class="accroche">Ventes<br>à emporter ?<span>C'est ici.</span></h1>
+                <form>
+                    <button class="bouton recherche">
+                        <svg class="icon"><use xlink:href="images/sprite.svg#loupe"></use></svg>
+                    </button>
+                    <input type="text" class="inputVille" placeholder="Essayez un nom de ville..."/>
+                </form>
+                <span class="separateur">— ou —</span>
+                <button class="bouton geoloc">
+                    Autour de moi
+                    <svg class="icon"><use xlink:href="images/sprite.svg#geoloc"></use></svg>
                 </button>
-            </form>
-            <span class="separateur">— ou —</span>
-            <button class="bouton geoloc">
-                Autour de moi
-                <svg class="icon"><use xlink:href="images/sprite.svg#geoloc"></use></svg>
-            </button>
+            </div>
         </div>
     </header>
 
--- resources/views/pages/map.blade.php
+++ resources/views/pages/map.blade.php
@@ -13,8 +13,28 @@
     <div id="map"></div>
 
     <div class="popup">
-        {{--@extends('layouts.store_card')--}}
-        @include('layouts.store_card')
+        <div class="popup_header">
+            <!--<svg class="icon"><use xlink:href="images/sprite.svg#loupe"></use></svg>
+            <svg class="icon"><use xlink:href="images/sprite.svg#loupe"></use></svg>-->
+        </div>
+        <div class="popup_body">
+            @include('layouts.store_card')
+            <div class="popup_info">
+                <div class="delivery">
+                    <h4>Livraison possible: </h4>
+                    <p>Non</p>
+                </div>
+                <div class="description">
+                    <h4>Description: </h4>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
+                </div>
+            </div>
+        </div>
+        <div class="popup_buttons">
+            <button><svg class="icon"><use xlink:href="images/sprite.svg#loupe"></use></svg>Catalogue</button>
+            <button><svg class="icon"><use xlink:href="images/sprite.svg#loupe"></use></svg>06 31 21 16 84</button>
+            <button><svg class="icon"><use xlink:href="images/sprite.svg#loupe"></use></svg>Itinéraire</button>
+        </div>
     </div>
 
 
