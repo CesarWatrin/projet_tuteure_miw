99e325dd2cc97287a7b3b62baf840c4b34c51916
maxBDL <maxime_didier@aol.com>
2021-02-02 15:06:44 +0100

    popup

--- public/js/app.js
+++ public/js/app.js
@@ -399,17 +399,6 @@ eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extr
 
 /***/ }),
 
-/***/ "./resources/sass/favoris.scss":
-/*!*************************************!*\
-  !*** ./resources/sass/favoris.scss ***!
-  \*************************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9yZXNvdXJjZXMvc2Fzcy9mYXZvcmlzLnNjc3M/MGIxMiJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQUEiLCJmaWxlIjoiLi9yZXNvdXJjZXMvc2Fzcy9mYXZvcmlzLnNjc3MuanMiLCJzb3VyY2VzQ29udGVudCI6WyIvLyBleHRyYWN0ZWQgYnkgbWluaS1jc3MtZXh0cmFjdC1wbHVnaW5cbmV4cG9ydCB7fTsiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./resources/sass/favoris.scss\n");
-
-/***/ }),
-
 /***/ "./node_modules/popper.js/dist/esm/popper.js":
 /*!***************************************************!*\
   !*** ./node_modules/popper.js/dist/esm/popper.js ***!
@@ -531,8 +520,7 @@ eval("// shim for using process in browser\nvar process = module.exports = {};\n
 /******/ 			["./resources/sass/app.scss"],
 /******/ 			["./resources/sass/map.scss"],
 /******/ 			["./resources/sass/navbar.scss"],
-/******/ 			["./resources/sass/connect.scss"],
-/******/ 			["./resources/sass/favoris.scss"]
+/******/ 			["./resources/sass/connect.scss"]
 /******/ 		];
 /******/ 		// no chunk on demand loading
 /******/ 		
--- resources/views/pages/map.blade.php
+++ resources/views/pages/map.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
-@extends('layouts.store_card')
+
 
 @section('content')
-    <!--<div class="searchbar">
+    <div class="searchbar">
         <div class="search">
             <input type="text" placeholder="Essayer un nom de ville ...">
             <button></button>
@@ -10,8 +10,22 @@
         <button class="filter"></button>
     </div>
 
-    <div id="map"></div>-->
+    <div id="map"></div>
 
+    <div class="popup">
+        {{--@extends('layouts.store_card')--}}
+        @include('layouts.store_card')
+    </div>
+
+
+
+    <script>
+        var map = document.getElementById('map')
+        var popup = document.getElementsByClassName('popup')[0];
+        map.addEventListener('click', () => {
+            popup.classList.toggle('active');
+        });
+    </script>
 @endsection
 
 
