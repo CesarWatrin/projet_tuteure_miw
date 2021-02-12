7d8a1fb810de4def7a7facfafa0fb7f6ae53f198
Yohan Salamone <yohan.salamone@outlook.com>
2021-02-12 10:31:34 +0100

    connexion fonctionnelle

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,12 +12,26 @@
   </div>
   <div class="formulaire">
     <h1 class="titreForm">Bienvenue</h1>
-    <form class="formulaireConnect">
-      <input type="email" id="email" class="inputText" placeholder="Adresse e-mail" required>
-      <input type="password" id="password" class="inputText" placeholder="Mot de passe" required>
+    <form method="POST" action="{{ route('login') }}" class="formulaireConnect">
+        @csrf
+        <input type="email" id="email" class="inputText" placeholder="Adresse e-mail"
+               name="email" value="{{ old('email') }}" autocomplete="email" autofocus required>
+        <input type="password" id="password" class="inputText" placeholder="Mot de passe"
+               name="password" required autocomplete="current-password" required>
 
-      <button type="button" class="button brouge">Se connecter</button>
-      <button type="button" class="button bgoogle"><img src="../images/google.svg" class="logoGoogle">Continuer avec Google</button>
+        @error('email')
+        <span class="input_error">
+                    <strong>{{ $message }}</strong>
+                </span>
+        @enderror
+        @error('password')
+        <span class="input_error">
+                    <strong>{{ $message }}</strong>
+                </span>
+        @enderror
+
+      <button type="submit" class="button brouge" >Se connecter</button>
+      <button type="button" class="button bgoogle"><svg class="logoGoogle"><use xlink:href="../images/sprite.svg#google"></use></svg>Continuer avec Google</button>
       <p class="ou">— ou —</p>
       <a href="{{ route('register') }}" class="button bbleu">S'inscrire</a>
     </form>
