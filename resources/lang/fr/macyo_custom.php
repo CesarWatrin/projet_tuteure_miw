306d33950f407df5d5e72b24f45747c5fe692276
Yohan Salamone <yohan.salamone@outlook.com>
2021-03-18 10:52:29 +0100

    suppression signalement

--- resources/lang/fr/macyo_custom.php
+++ resources/lang/fr/macyo_custom.php
@@ -14,7 +14,18 @@ return [
     'store' => 'Commerce',
     'rating_value' => 'Note',
     'comment' => 'Commentaire',
-    'reported' => 'Signalé'
+    'reported' => 'Signalé',
+    'reported_only' => 'Commentaires signalés uniquement',
 
+    'remove_report' => 'Suppr. signalement',
+    'information' => 'Information',
+    // Confirmation messages and bubbles
+    'remove_report_confirm'                              => 'Vous vous apprêtez à supprimer le signalement pour ce commentaire. Veuillez confirmer l\'opération.',
+    'remove_report_confirmation_title'                   => 'Signalement supprimé',
+    'remove_report_confirmation_message'                 => 'Le signalement pour ce commentaire a été supprimé avec succès.',
+    'remove_report_confirmation_not_title'               => 'Signalement NON supprimé',
+    'remove_report_confirmation_not_message'             => 'Une erreur est survenue. Le signalement n’a peut-être pas été supprimé.',
+    'remove_report_confirmation_not_deleted_title'       => 'Signalement non supprimé',
+    'remove_report_confirmation_not_deleted_message'     => 'Aucune modification. Le signalement a été conservé.',
 
 ];
